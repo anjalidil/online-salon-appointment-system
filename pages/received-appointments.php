31e7c8e77840e01a6c200684c00f8390e5554364
Anjali Jayasinghe <61909890+anjalidil@users.noreply.github.com>
2021-06-02 16:35:30 +0530

    Update received-appointments.php

--- pages/received-appointments.php
+++ pages/received-appointments.php
@@ -233,8 +233,8 @@
                       $mail->isSMTP();                                            // Send using SMTP
                       $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
                       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                      $mail->Username   = 'salondarknshine@gmail.com';                     // SMTP username
-                      $mail->Password   = '10darknshine10';                               // SMTP password
+                      $mail->Username   = 'salonEmail@gmail.com';                     // SMTP username
+                      $mail->Password   = '';                               // SMTP password
                       $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
                   
