00a23dc1d074dd18b3faed69d34ffbf654106e3d
Anjali Jayasinghe <61909890+anjalidil@users.noreply.github.com>
2021-06-02 16:37:55 +0530

    Update received-appointments.php

--- pages/received-appointments.php
+++ pages/received-appointments.php
@@ -239,7 +239,7 @@
                       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
                   
                       //Recipients
-                      $mail->setFrom('salondarknshine@gmail.com', 'Salon Dark&Shine');
+                      $mail->setFrom('salonEmail@gmail.com', 'Salon Dark&Shine');
                       $mail->addAddress($_POST["cus_email"]);     // Add a recipient
                      
                   
@@ -279,13 +279,13 @@
                       $mail->isSMTP();                                            // Send using SMTP
                       $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
                       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                      $mail->Username   = 'salondarknshine@gmail.com';                     // SMTP username
-                      $mail->Password   = '10darknshine10';                               // SMTP password
+                     $mail->Username   = 'salonEmail@gmail.com';                     // SMTP username
+                      $mail->Password   = '';                               // SMTP password
                       $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
                   
                       //Recipients
-                      $mail->setFrom('salondarknshine@gmail.com', 'Salon Dark&Shine');
+                      $mail->setFrom('salonEmail@gmail.com', 'Salon Dark&Shine');
                       $mail->addAddress($_POST["cus_email"]);     // Add a recipient
                      
                   
